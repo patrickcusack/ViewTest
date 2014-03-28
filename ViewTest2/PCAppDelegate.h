125bc8f5dce6984e5d01f95ca814175343b73a30
patrickcusack <patrickcusack@mac.com>
2014-03-28 11:09:00 -0700

    Initial Commit

--- ViewTest2/PCAppDelegate.h
+++ ViewTest2/PCAppDelegate.h
@@ -1,15 +1,22 @@
 //
 //  PCAppDelegate.h
-//  ViewTest2
+//  ViewTest
 //
-//  Created by Patrick Cusack on 3/28/14.
+//  Created by Patrick Cusack on 3/27/14.
 //  Copyright (c) 2014 Patrick Cusack. All rights reserved.
 //
 
 #import <Cocoa/Cocoa.h>
 
-@interface PCAppDelegate : NSObject <NSApplicationDelegate>
+@interface PCAppDelegate : NSObject <NSApplicationDelegate>{
+    NSRect originalRect;
+    BOOL isMoved;
+}
 
 @property (assign) IBOutlet NSWindow *window;
+@property (weak) IBOutlet NSTextField *label;
+
+- (IBAction)update:(id)sender;
+- (IBAction)move:(id)sender;
 
 @end
--- ViewTest2/PCAppDelegate.m
+++ ViewTest2/PCAppDelegate.m
@@ -1,18 +1,40 @@
 //
 //  PCAppDelegate.m
-//  ViewTest2
+//  ViewTest
 //
-//  Created by Patrick Cusack on 3/28/14.
+//  Created by Patrick Cusack on 3/27/14.
 //  Copyright (c) 2014 Patrick Cusack. All rights reserved.
 //
 
 #import "PCAppDelegate.h"
+
 
 @implementation PCAppDelegate
 
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification
 {
     // Insert code here to initialize your application
+    
+    originalRect = [[self label] frame];
+    [[self label] setWantsLayer:YES];
+    isMoved = NO;
+}
+
+- (IBAction)update:(id)sender {
+    [[[self label] layer] setNeedsDisplay];
+}
+
+- (IBAction)move:(id)sender {
+    
+    if (isMoved) {
+        [[[self label] animator] setFrame:originalRect];
+    } else {
+        NSRect rect = [[self label] frame];
+        rect.size.width *= 2.0;
+        [[[self label] animator] setFrame:rect];
+    }
+    
+    isMoved = !isMoved;
 }
 
 @end
